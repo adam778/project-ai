0c785b2f5ce5327e40be79e9c8e74c5c0342715b
Piotr K <kacperczyk.piotr@gmail.com>
2017-12-05 22:55:21 +0100

    Resolve #6

--- src/main/java/com/ai/ai/dto/ClientDto.java
+++ src/main/java/com/ai/ai/dto/ClientDto.java
@@ -4,18 +4,34 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Builder
 @Getter
 @Setter
 public class ClientDto {
+    //Zakładam, że klient zawsze ma imię i nazwisko a nazwa firmy jest opcjonalna.
+
+    @NotNull(message = "Client id can't be null")
     Long id;
 
+    @NotBlank(message = "Client name can't be empty")
     String name;
+
+    @NotBlank(message = "Client surname can't be empty")
     String surname;
+
     String companyName;
 
+    @NotBlank(message = "Client adress can't be empty")
     String adress;
+
+    @NotBlank(message = "Client phon can't be empty")
     String phone;
+
+    @Email(message = "Client email is invalid")
     String email;
 
 }
--- src/main/java/com/ai/ai/dto/ItemDto.java
+++ src/main/java/com/ai/ai/dto/ItemDto.java
@@ -5,12 +5,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Builder
 @Getter
 @Setter
 public class ItemDto {
-
+    @NotNull(message = "Item id can't be null")
     Long id;
-
+    @NotBlank(message = "Item name can't be empty")
     String name;
 }
